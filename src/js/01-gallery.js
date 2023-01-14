ff241babebca8187a652e5ffc5bec5924d8d1d20
EvgenyVeliki <108056667+EvgenyVeliki@users.noreply.github.com>
2023-01-14 10:45:41 +0200

    Test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+hello = "Hello World"
+console.log(hello)
 console.log(galleryItems);
