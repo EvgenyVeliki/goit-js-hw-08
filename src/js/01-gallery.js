ff4cecd33d91cf79a432b5c0f5640bea7016871f
EvgenyVeliki <108056667+EvgenyVeliki@users.noreply.github.com>
2023-01-17 21:36:08 +0200

    HomeWork 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
-hello = "Hello World"
-console.log(hello)
-console.log(galleryItems);
+
+
+
+const boxGalleryItems = document.querySelector('.gallery')
+
+const listItemsMarkup = createListItemMarkup(galleryItems)
+
+
+boxGalleryItems.innerHTML = listItemsMarkup
+
+
+
+function createListItemMarkup (items) {
+    return items.map(item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-caption="${item.description}" />
+  </a>`).join('')
+}
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",
+captionDelay: 250,});t
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+
+dataFromLocalStorage();
+
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  console.log(formData);
+  formData = {};
+}
+
+function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (data) {
+    email.value = data.email || '';
+    message.value = data.message || '';
+  }
+}
